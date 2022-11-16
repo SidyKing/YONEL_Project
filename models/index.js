56e7211ec279621d230d930a5f00972ada9289c7
Sidy Traore <generaltra10@gmail.com>
2022-11-16 23:45:59 +0000

    database config

---
+++ models/index.js
@@ -0,0 +1,29 @@
+const dbConfig = require("../config/db.config");
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  port: "8889",
+  operatorsAliases: false,
+
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  }
+});
+
+
+//notre base de données
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+//Definir les tables de la base de données
+
+
+module.exports = db;
